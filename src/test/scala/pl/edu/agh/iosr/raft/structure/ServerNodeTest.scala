81edd45ec2f9d98d7448934ab161a685f1526411
Dawid Pawlak <dpawlak1992@gmail.com>
2016-11-03 18:05:30 +0100

    trivial tests to test travis integration

---
+++ src/test/scala/pl/edu/agh/iosr/raft/structure/ServerNodeTest.scala
@@ -0,0 +1,22 @@
+package pl.edu.agh.iosr.raft.structure
+
+import org.scalatest.{Matchers, WordSpecLike}
+
+
+class ServerNodeTest extends WordSpecLike with Matchers{
+
+  "this test" should {
+    "pass" in {
+      true shouldEqual true
+    }
+  }
+
+  "this test" should {
+    "fail" in {
+      true shouldEqual false
+    }
+
+  }
+}
+
+
